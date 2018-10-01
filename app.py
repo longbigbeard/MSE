0804097ba5b783df90851caed60d0470476b6125
yema <yema2358@163.com>
2018-10-01 22:30:13 +0800

    mse 2019/10/1

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, url_for
+import config
+
+app = Flask(__name__)
+app.config.from_object(config)
+
+
+@app.route('/')
+def index():
+    return render_template('')
+
+
+if __name__ == '__main__':
+    app.run()
